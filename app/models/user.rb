6b0e7dbcf976a5ab318a532c64803a0eb0f8ddb9
Devin Miller <email": null>
2017-10-11 11:40:31 -0700

    Add User validations, BCrypt, and authentication and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :name, :email, :password_hash, presence: true
+  validates :email, uniqueness: true
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, given_password)
+    user = User.find_by_email(email)
+    return nil if user == nil
+    if user.password == given_password
+      return user
+    else
+      return nil
+    end
+  end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe User do
+  let!(:user) { User.create!(name: "Marguerite Fish", email: "fish@llama.com", password: "ham")}
+
+  describe 'validations' do
+    context 'it is invalid when' do
+      it 'does not have a name' do
+        user.name = nil
+        expect(user).to_not be_valid
+      end
+      it 'does not have an email' do
+        user.email = nil
+        expect(user).to_not be_valid
+      end
+      it 'has a non-unique email' do
+        user2 = User.new(name: "Marguerite Fish", email: "fish@llama.com", password: "ham")
+        expect(user2).to_not be_valid
+      end
+      it 'does not have a password_hash' do
+        user.password_hash = nil
+        expect(user).to_not be_valid
+      end
+    end
+    context 'it is valid when' do
+      it 'has a name, email, and password_hash' do
+        expect(user).to be_valid
+      end
+    end
+  end
+
+  describe 'authentication' do
+    it 'has a hashed password' do
+      expect(user.password_hash).to_not eq "ham"
+    end
+    it 'returns the user when they correctly authenticate' do
+      expect(User.authenticate("fish@llama.com", "ham")).to eq user
+    end
+    it 'returns nil when the user enters an email which does not exist in the database' do
+      expect(User.authenticate("marvin@ham.com", "ham")).to be_nil
+    end
+    it 'returns nil when the password does not match' do
+      expect(User.authenticate("fish@llama.com", "spam")).to be_nil
+    end
+  end
+end
